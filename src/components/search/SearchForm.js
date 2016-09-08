0dcd74b50d55d0a0dfb8d48cc9ba519be8f2fc09
hui-w <huiwang@qlike.com>
2016-09-08 17:20:20 +0800

    update search form

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -1,15 +1,26 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import '../../styles/search-form.css';
 
-const SearchForm = ({ onSearch }) => (
-  <div className="search-form">
-    <form onSubmit={() => onSearch("Pay")}>
-      SearchForm
-      <input type="submit" value="Search" />
-    </form>
-  </div>
-);
+class SearchForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const onSearch = this.props.onSearch;
+    onSearch("pay");
+  }
+
+  render() {
+    const onSearch = this.props.onSearch;
+    return (
+      <div className="search-form">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value="" />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    );
+  }
+}
 
 SearchForm.propTypes = {
   onSearch: PropTypes.func.isRequired
